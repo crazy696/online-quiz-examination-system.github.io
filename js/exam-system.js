e30534e57cea98461e87fcba5bfa53670fe0aac0
CRAZY696 <46396562+crazy696@users.noreply.github.com>
2024-01-05 14:26:03 +0800

    Update exam-system.js
    增加随机出题

--- js/exam-system.js
+++ js/exam-system.js
@@ -160,10 +160,10 @@ $(function () {
 
 
 function shuffle(arr) {
-  let newArr = [...arr];
-  for (let i = newArr.length - 1; i > 0; i--) {
+    let newArr = [...arr];
+    for (let i = newArr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
-  }
-  return newArr;
+    }
+    return newArr;
 }
